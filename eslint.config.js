26ebe35200d37b6ca6c09d2b983a379d02e7d81d
Tymek Słobodzian <tymekito@interia.eu>
2025-06-13 11:38:06 +0200

    fix linter

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 
 export default defineConfig([
-  { ignores: ["dist"] },
+  { ignores: ["dist", "keycloak-ibos-ui-theme/**"] },
   {
     name: "app/src",
     files: ["**/*.{js,mjs,jsx,vue}"],
@@ -19,11 +19,48 @@ export default defineConfig([
   {
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
     },
   },
 
   js.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+
+  // Twoje custom reguły
+  {
+    rules: {
+      "linebreak-style": 0,
+      "object-curly-spacing": [2, "always"],
+      "require-jsdoc": 0,
+      "max-len": "off",
+      eqeqeq: 2,
+      camelcase: "error",
+      "no-undef-init": "error",
+      "block-scoped-var": "error",
+      "no-extra-semi": "error",
+      "no-empty": "error",
+      "no-func-assign": "error",
+      "no-unreachable": "error",
+      "no-console": "warn",
+      "vue/no-v-for-template-key": "off",
+      "vue/no-v-model-argument": "off",
+      "vue/no-multiple-template-root": 0,
+      "padding-line-between-statements": [
+        "error",
+        {
+          blankLine: "always",
+          prev: "*",
+          next: "export",
+        },
+      ],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+    },
+  },
+
   skipFormatting,
 ]);
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -6,7 +6,6 @@ export const useCounterStore = defineStore("counter", () => {
   const doubleCount = computed(() => count.value * 2);
   function increment() {
     count.value++;
-    console.log("XD");
   }
 
   return { count, doubleCount, increment };
